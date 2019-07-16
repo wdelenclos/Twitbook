8d8b2ae697e6bfc5bc748f89019c10225b9e57a4
Dylan Settbon <dylans_set@hotmail.fr>
2019-07-16 12:26:59 +0200

    [ADD] Change gitignore and add config.js

---
+++ config.js
@@ -0,0 +1,22 @@
+((document) => {
+    // const firebaseConfig ={
+    //     apiKey: "AIzaSyABQzGFeUvLcEKYG7wecBzY38jjxr1QmmY",
+    //     authDomain: "pwaproject-8bc83.firebaseapp.com",
+    //     databaseURL: "https://pwaproject-8bc83.firebaseio.com",
+    //     projectId: "pwaproject-8bc83",
+    //     storageBucket: "pwaproject-8bc83.appspot.com",
+    //     messagingSenderId: "46118763043",
+    //     appId: "1:46118763043:web:6aa634322e0e9a5c"
+    //   };
+    const firebaseConfig = {
+        apiKey: "AIzaSyCxGDYdT0_AtDrvwpjAxTpMOKFTEWi8eFI",
+        authDomain: "iw-1-firebase.firebaseapp.com",
+        databaseURL: "https://iw-1-firebase.firebaseio.com",
+        projectId: "iw-1-firebase",
+        storageBucket: "iw-1-firebase.appspot.com",
+        messagingSenderId: "809504521421",
+        appId: "1:809504521421:web:0a20f0895a2b914a"
+    }
+
+    document.config = firebaseConfig;
+})(document);
